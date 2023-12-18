73cc636c85d557fcc01c2ce8719339d2bb137607
Michal Moudrý <moudry.michal@live.com>
2023-12-18 12:29:55 +0100

    Finished math operations chapter

--- manual/math_operations.jl
+++ manual/math_operations.jl
@@ -15,3 +15,18 @@ println(temp1, temp2)
 
 rounded = round(Int8,127.4)
 println(typeof(rounded), rounded)
+
+test = 123 ⊽ 124
+println(test, " - ", typeof(test))
+
+# Vectorized "dot" operators
+test_array = [1,2,3]
+computed_array = test_array .^ 3
+println(test_array, " => ", computed_array)
+computed_array = (test_array .+ 1) .*2
+println(test_array, " => ", computed_array)
+computed_array = sin.(test_array)
+println(test_array, " => ", computed_array)
+
+# Operator Procedence and Associativity
+println(Base.operator_precedence(:+), Base.operator_precedence(:*))
