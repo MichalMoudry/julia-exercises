0b10aeaaec2936b10a532aaad92a94c3d0ae5a20
Michal Moudrý <moudry.michal@live.com>
2023-12-21 15:53:45 +0100

    Finished string chapter

--- manual/string.jl
+++ manual/string.jl
@@ -10,3 +10,34 @@ println(str[begin], str[1], str[end-1], str[1:5])
 substr = SubString(str, 1, 5)
 println(substr, typeof(substr))
 println(chomp(str))
+
+# Unicode and UTF-8
+s = "\u2200 x \u2203 y"
+println(s)
+for i in s
+    println(i)
+end
+println(collect(eachindex(s)))
+
+# Concatenation + interpolation
+greet = "Hello"
+whom = "world"
+res = string(greet, ", ", whom, ".\n")
+println(res, "$greet, $whom.")
+
+# Regular expressions
+re = r"^\s*(?:#|$)"
+println(typeof(re))
+println(
+    occursin(r"^\s*(?:#|$)", "not a comment"),
+    occursin(r"^\s*(?:#|$)", "# a comment")
+)
+m = match(r"^\s*(?:#|$)", "# a comment")
+if m === nothing
+    println("not a comment")
+else
+    println("blank or a comment")
+end
+
+m = match(r"^\s*(?:#\s*(.*?)\s*$|$)", "# a comment ")
+println(m, m.captures)
